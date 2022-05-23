307756cd43ba285e2c60da606fafc5980752b25b
raza <raza.zech@gmail.com>
2022-05-24 00:17:32 +0500

    first code

---
+++ src/test/java/learning/mavenn/day1.java
@@ -0,0 +1,26 @@
+package learning.mavenn;
+
+import org.testng.annotations.Test;
+
+public class day1 {
+	
+	
+	@Test
+	
+	public void print() {
+		
+		
+		System.out.println("hello");
+		
+	}
+	
+	@Test
+	
+	public void prtinhello() {
+		
+		
+	System.out.println("world");
+		
+	}
+
+}
---
+++ src/test/java/learning/mavenn/first.java
@@ -0,0 +1,18 @@
+package learning.mavenn;
+
+import org.testng.annotations.Test;
+
+public class first {
+	
+	@Test
+	
+
+	public void dsf() {
+		
+		System.out.println("hello");
+		
+		
+		
+	}
+
+}
